8807d1b616f23db275307a4a5e593f876026608c
aydenscott <86305521+aydenscott@users.noreply.github.com>
2021-07-08 18:16:50 -0400

    Update hide.py

--- hide.py
+++ hide.py
@@ -1,12 +1,13 @@
-import time
+import time, os
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
-from config import red_username, red_password
 
 source = r"https://www.reddit.com/login/?dest=https%3A%2F%2Fwww.reddit.com%2F"
 driver = webdriver.Chrome()
 driver.get(source)
 driver.maximize_window()
+username = os.environ.get('red_username')
+password = os.environ.get('red_password')
 
 
 # Log in
